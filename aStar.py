6c1052f0e55f7bdc3627718f16763e3db17ed47e
Omar D. Ramirez <omardramirez2002@gmail.com>
2023-02-08 00:54:41 -0600

    This commit adds the reset functionality when pressing c

--- aStar.py
+++ aStar.py
@@ -209,9 +209,6 @@ def main(win, width):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
-            if started:
-                continue
 
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
@@ -240,12 +237,17 @@ def main(win, width):
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for square in row:
                             square.updateNeigbors(grid)
                     
                     algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = makeGrid(ROWS, width)
 
     pygame.quit()
 
